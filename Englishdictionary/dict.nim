6a7b4a27ce377371102027f387931a14fbe7ce83
Manoj Pawar SJ <46449199+manojpawarsj12@users.noreply.github.com>
2019-11-09 14:30:44 +0530

    add functions

--- Englishdictionary/dict.nim
+++ Englishdictionary/dict.nim
@@ -1,12 +1,16 @@
 import json
 
-var a=parseFile("data.json")
+proc meaning(b: string): JsonNode=
+    var a=parseFile("data.json")
+    try:
+        return a[b]
+    except KeyError:
+        echo "we didnt understand your query check speling mistake \n"
 
-echo "Enter the word that you want meaning \n"
+echo "English Dictionary Using Nim \nEnter the word "
 var b=readLine(stdin)
-try:
-    var c=a[b]
-    echo c
-except KeyError:
-    echo "we didnt understand your query check speliing mistake \n"
+echo meaning(b)
+
+
+
 
